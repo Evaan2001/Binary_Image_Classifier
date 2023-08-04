81a4b2fb4445aa63928f3cb16bcf8b4b595cce93
Evaan Yaser Ahmed <ahmedeva@grinnell.edu>
2023-08-04 17:28:32 -0500

    Update Binary_Image_Classifier.py

--- Binary_Image_Classifier.py
+++ Binary_Image_Classifier.py
@@ -1,16 +1,21 @@
+"""
+File that builds and trains a CNN for binary image classification.
+"""
+
+# Import packages
+
 import matplotlib.pyplot as plt
 import os
 
 plt.style.use('classic')
 
-#import packages
 import tensorflow as tf
 from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 
-#now import my Essentials.py file that has a bunch of helper functions
+# Now import my Essentials.py file that has a bunch of helper functions
 
 import Essentials
 ###################################
